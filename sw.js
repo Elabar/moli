04ddb5b875d41f6dd5bc0d1bea2ebcf79886c1df
Elabar <leehoemun@gmail.com>
2019-08-29 02:04:30 +0800

    added offline ability

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('moliAssistant').then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/index',
+            '/meonly',
+            '/detetor',
+            '/custom',
+            '/chat',
+            '/404'
+        ]).then(()=>self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open('moliAssistant')
+        .then(cache => cache.match(event.request, {ignoreSearch:true}))
+        .then(response => {
+            return response || fetch(event.request)
+        })
+    );
+});
